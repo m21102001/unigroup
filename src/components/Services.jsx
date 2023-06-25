976de5714fe7b6960b1da408b53bfcc426e6aa23
m21102001 <mohammedtarek21102001@gmail.com>
2023-06-25 14:37:34 +0300

    servies section
    servies section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,8 +1,35 @@
+import { Button } from "react-bootstrap"
+import servicestools from "../assets/navbar/services_tools.png"
 import "./servivce.css"
 const Services = () => {
   return (
-    <div className="serivce">
-        <h1></h1>
+    <div className="p-3">
+      <h2 className="serivce pt-5">serivce</h2>
+      <div className="container text-center">
+        <div className="row align-items-start services-section">
+          <div className="col-6">
+            <h3 className="whatwedo text-start">WHAT WE DO</h3>
+            <p className="Wearehere">We are here to help you develop yourself</p>
+            <span className="loerm pt-5">Sed ut perspiciatis unde omnis iste natus error sit of the volum
+              accusantium loLorem ipsum dolor sit amet, consectetur adcing elit,
+              sed do eiusmod tempor incdunt ut labore et dolore magna aliqua.
+              totam rem aperiam, eaque ipsa quae ab illo incdunt ut labore et dolore magna aliqua.
+              totam rem aperiam, eaque ipsa quae ab illoinvento vatis quasi architecto beatae vitae dicta.</span>
+            <div className="pt-4 text-start">
+              <Button variant="outlit" className="btn btn-orange">Start New Project</Button>
+            </div>
+          </div>
+          <div className="col-6">
+            <div className="row">
+              <img src={servicestools} alt="servicestools" />
+              {/* <div className="col-6">col-8</div>
+              <div className="col-6">col-4</div> */}
+            </div>
+
+          </div>
+
+        </div>
+      </div>
     </div>
   )
 }
