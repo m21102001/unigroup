04c6157cc85eaf4c382e716827d02f5d211682e3
m21102001 <mohammedtarek21102001@gmail.com>
2023-06-25 15:32:21 +0300

    updated
    updated

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import progress from "../assets/navbar/progress.png"
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
