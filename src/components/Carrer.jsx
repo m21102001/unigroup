6a484f58a82775889a0e400f24f13e765de76687
aymanhossam55 <100770611+aymanhossam55@users.noreply.github.com>
2023-06-26 13:48:28 +0300

    modify

--- src/components/Carrer.jsx
+++ src/components/Carrer.jsx
@@ -16,17 +16,17 @@ function Carrer() {
 
     <Container>
       <Row>
-        <Col><ButtonGroup className="department me-5" aria-label="First group">
+        <Col><ButtonGroup className="department first-group me-5" aria-label="First group">
         <Button className='buttonGroup' variant='' active>Jobs</Button> 
       </ButtonGroup>
       </Col>
-        <Col><ButtonGroup className="department me-5" aria-label="First group">
+        <Col><ButtonGroup className="department first-group me-5" aria-label="First group">
         <Button className='buttonGroup' variant='' >Internship</Button> 
       </ButtonGroup>
       </Col>
       </Row>
       <Row>
-      <Col aria-colspan={2}><Card style={{ width: '18rem' }}>
+      <Col aria-colspan={3}><Card style={{ width: '18rem' }}>
       <Card.Body>
         <Card.Title>Web Developer</Card.Title>
         <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
@@ -36,8 +36,7 @@ function Carrer() {
       </ButtonGroup>
       </Card.Body>
     </Card></Col>
-      {/* <Col>2 of 2</Col> */}
-      <Col aria-colspan={2}><Card style={{ width: '18rem'  }}>
+      <Col aria-colspan={3}><Card style={{ width: '18rem'  }}>
       <Card.Body>
         <Card.Title>UI/UX Designer</Card.Title>
         <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
@@ -50,23 +49,26 @@ function Carrer() {
     <Col>
     <div className='vr'></div>
     </Col>
-      {/* <Col></Col> */}
-      <Col aria-colspan={4}>
-        <h1 className='card-head'>UI/UX Designer</h1>
-        <h2 className='card-title'>Requirments</h2>
-        <p className='description'>Lorem ipsum dolor sit amet, consectetur 
-        adipiscing elit, sed do eiusmod tempor 
-        incididunt ut labore et dolore magna aliqua.
-        quis nostrud exercitation ullamco laboris nisi ut aliquip 
-        ex ea commodo consequat. Duis aute irure dolor 
-        in reprehenderit in voluptate velit esse cillum dolore eu </p>
-        <h2 className='card-title'>Requirments</h2>
-        <p className='description'>Lorem ipsum dolor sit amet, consectetur 
-            adipiscing elit, sed do eiusmod tempor 
-            incididunt ut labore et dolore magna aliqua.
-            quis nostrud exercitation ullamco laboris nisi ut aliquip 
-            ex ea commodo consequat. Duis aute irure dolor 
-            in reprehenderit in voluptate velit esse cillum dolore eu </p>
+      <Col aria-colspan={6} className='carrer-internship'>
+        <h1 className='head'>UI/UX Designer</h1>
+        <h2 className='title'>Requirments</h2>
+        
+        <li className='description'>Lorem ipsum dolor sit amet, consectetur</li> 
+        <li className='description'>adipiscing elit, sed do eiusmod tempor </li> 
+        <li className='description'>incididunt ut labore et dolore magna.</li>
+        <li className='description'>quis nostrud exercitation ullamco laboris</li>
+        <li className='description'>ex ea commodo consequat. Duis aute</li>
+        <li className='description'>in reprehenderit in voluptate velit esse</li>
+
+        <h2 className='title'>Requirments</h2>
+        
+        <li className='description'>Lorem ipsum dolor sit amet, consectetur</li> 
+        <li className='description'>adipiscing elit, sed do eiusmod tempor </li> 
+        <li className='description'>incididunt ut labore et dolore magna</li>
+        <li className='description'>quis nostrud exercitation ullamco laboris</li>
+        <li className='description'>ex ea commodo consequat. Duis aute</li>
+        <li className='description'>in reprehenderit in voluptate velit esse</li>
+        
             <h4>Job title:</h4><span>lorem ipsum</span>
             <h4>Job Location:</h4><span>lorem ipsum</span>
             <h4>Skill Required:</h4><span>Adobe Photoshop, Illustrator, sketch, Typography</span>
