dd9b4f5a6fc6f8a1a68f688b455f74f4146cbcd2
Adel Ahmed <81045048+adelahmed0@users.noreply.github.com>
2023-06-26 17:28:59 +0300

    fix header
    fix header

--- src/pages/Careers.jsx
+++ src/pages/Careers.jsx
@@ -1,16 +1,24 @@
 import Carrer from "../components/Carrer"
 import Footer from "../components/Footer"
 import NavScrollExample from "../components/NavScrollExample"
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos.js";
 
 
 const Careers = () => {
-  return (
-    <div id="Careers">
-      <NavScrollExample/>
-      <Carrer />      
-      <Footer />
-    </div>
-  )
+    return (
+        <div id="Careers">
+            <div className='bg-careers'>
+                <NavScrollExample/>
+                <div className='heading-c'>
+                    <h1>We’re Hiring!</h1>
+                    <p>We’re looking for the best talent and we’re offering a special benefits package in return.
+                        If you think you got what it takes to be the best, apply now...</p>
+                </div>
+            </div>
+            <Carrer/>
+            <Footer/>
+        </div>
+    )
 }
 
 export default Careers
