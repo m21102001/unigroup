e2f600ac4a097f9698c86fb929a04928040517e2
Adel Ahmed <81045048+adelahmed0@users.noreply.github.com>
2023-06-26 17:51:30 +0300

    fix paragraph
    fix paragraph

--- src/pages/Careers.jsx
+++ src/pages/Careers.jsx
@@ -1,18 +1,29 @@
 import Carrer from "../components/Carrer"
 import Footer from "../components/Footer"
 import NavScrollExample from "../components/NavScrollExample"
-import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos.js";
+import {useState} from "react";
 
 
 const Careers = () => {
+    const [isShowMore, setIsShowMore] = useState(false);
+
+    const toggleReadMoreLess = () => {
+        setIsShowMore(!isShowMore);
+    };
     return (
         <div id="Careers">
             <div className='bg-careers'>
                 <NavScrollExample/>
                 <div className='heading-c'>
                     <h1>We’re Hiring!</h1>
-                    <p>We’re looking for the best talent and we’re offering a special benefits package in return.
-                        If you think you got what it takes to be the best, apply now...</p>
+                    <p>We’re looking for the best talent and we’re offering a special {isShowMore && (
+                        <>
+                            benefits package in return.
+                            If you think you got what it takes to be the best, apply now...
+                        </>
+                    )}</p>
+
+                    <span onClick={toggleReadMoreLess}>{isShowMore ? "Read Less" : "Read More"}</span>
                 </div>
             </div>
             <Carrer/>
