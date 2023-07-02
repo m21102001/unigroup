7d997e821d3dd0481c2b7f8d5f7a2ad1515bc09e
FatimaOElfar <105023261+FatimaOElfar@users.noreply.github.com>
2023-07-02 19:22:13 +0300

    #latest-pick

--- src/components/LastestPacks.jsx
+++ src/components/LastestPacks.jsx
@@ -1,36 +1,82 @@
 import "./Lastest.css"
-import ToolbarBasicExample from "./ToolbarBasicExample"
-import department from "../assets/navbar/department.png"
+// import ToolbarBasicExample from "./ToolbarBasicExample"
+// import department from "../assets/navbar/department.png"
 // import { Button, ButtonGroup } from "react-bootstrap"
+import { useState,useEffect } from "react"
+
+import axios from "axios"
 const LastestPacks = () => {
+
+    const url_api="https://fakestoreapi.com/products"
+    const [images ,setImages]=useState([])
+    useEffect(()=>{
+        axios.get(url_api)
+        .then((res)=>{
+            setImages(res.data)
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+    })
   return (
-    <>
-    <h2 className="LastestPicks">Lastest Picks</h2>
-    <p className="check-our">check our latest news project</p>
-    <ToolbarBasicExample/>
-    {/* <div className="container text-center">
-  <div className="row">
-    <div className="col">
-    <ButtonGroup className="department me-5" aria-label="First group">
-        <Button className='buttonGroup' variant='' active>All</Button> 
-      </ButtonGroup>
-    </div>
-    <div className="col">
-      Column
-    </div>
-    <div className="col">
-      Column
-    </div>
-    <div className="col">
-      Column
-    </div>
-    <div className="col">
-      Column
+
+    <div className="content">
+    <div className="container">
+      <div className="head">
+          <h1>LATEST PICKS</h1>
+          <p>check our latest news project</p>
+      </div>
+      <div className="header">
+        <ul>
+          <li>
+            <a href="./all" className="all"> <button>All</button> </a>
+          </li>
+          <li>
+       <a href="./marketing"> <button>Marketing</button></a>
+          </li>
+          <li>
+          
+          <a href="./business"><button>Business</button></a>
+        
+
+          </li>
+          <li>
+           <a href="./development"><button>Development</button> </a>
+          </li>
+          <li>
+            <a href="./art"> <button>Art & Designe</button></a>
+          </li>
+        </ul>
+      </div>
+      <div className="image">
+          {
+              images.map((data)=>{
+                  return (
+                      <div className="image-data" key={data.id}><img src={data.image} alt ="an error ocuure"/></div>
+                  )
+
+              })
+          }
+
+      </div>
+
     </div>
   </div>
-</div> */}
-    <img src={department} alt="department" className="image-depart" />
-    </>
+
+
+
+
+
+
+
+
+    // <>
+    // <h2 className="LastestPicks">Lastest Picks</h2>
+    // <p className="check-our">check our latest news project</p>
+    // <ToolbarBasicExample/>
+
+    // <img src={department} alt="department" className="image-depart" />
+    // </>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import Footer from "../components/Footer"
 // import ToolbarBasicExample from "../components/ToolbarBasicExample"
 import Team from "../components/Team"
 import Header from "../components/Header";
-import Aboutus from "../components/Aboutus"
+import Aboutus from "../components/Aboutus";
 import Company from "../components/Company";
 
 const Home = () => {
@@ -26,3 +26,4 @@ const Home = () => {
 }
 
 export default Home;
+
