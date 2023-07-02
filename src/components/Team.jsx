562364e104d7e67731409d2961da5b9d5680bbea
m21102001 <mohammedtarek21102001@gmail.com>
2023-07-02 17:19:51 +0300

    fixed card (one row don't tow rows) ayman
    fixed card (one row don't tow rows)

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -13,119 +13,159 @@ import Behance from "../assets/navbar/Behance.png"
 
 
 function Team() {
-    return (
-        <div>
-        <h1 className='heading'>Our Team</h1>
-        <ButtonToolbar className='contant-center justify-content-center' aria-label="Toolbar with button groups">
-          <ButtonGroup className="department me-5" aria-label="First group">
-            <Button className='buttonGroup' variant='' active>All</Button> 
-          </ButtonGroup>
-          <ButtonGroup className="department me-5" aria-label="Third group">
-            <Button className='buttonGroup' variant='outline-dark'>Owner</Button>
-          </ButtonGroup>
-          <ButtonGroup className="department me-5" aria-label="Third group">
-            <Button className='buttonGroup' variant='outline-dark'>UI/UX</Button>
-          </ButtonGroup>
-          <ButtonGroup className="department me-5" aria-label="Third group">
-            <Button className='buttonGroup' variant='outline-dark'>Front-End</Button>
-          </ButtonGroup>
-          <ButtonGroup className="department me-5" aria-label="Third group">
-            <Button className='buttonGroup' variant='outline-dark'>Back-End</Button>
-          </ButtonGroup>
-          <ButtonGroup className="department me-5" aria-label="Third group">
-            <Button className='buttonGroup' variant='outline-dark'>Flutter</Button>
-          </ButtonGroup>
-        </ButtonToolbar>
-        <Container className=' d-flex flex-column justify-content-center align-items-center'>
-        <Row  className='row row-team ms-4'>
-    <Card style={{ width: '15rem' , margin:"2rem" }}>
-        <img src={team} alt="" />
-      <Card.Body>
-        <Card.Title>Mohamed Ali</Card.Title>
-        <Card.Text className='text'>Graphic Design</Card.Text>
-        <img src={linkedin} alt="" />
-        <img src={Github} alt="" />
-        <img src={Behance} alt="" />
-      </Card.Body>
-    </Card>
-    <Card style={{ width: '15rem' , margin: "2rem" }}>
-        <img src={team} alt="" />
-      <Card.Body>
-        <Card.Title >Mohamed Ali</Card.Title>
-        <Card.Text className='text'>Head of Flutter</Card.Text>
-        <img src={linkedin} alt="" />
-        <img src={Github} alt="" />
-        <img src={Behance} alt="" />
-      </Card.Body>
-    </Card>
-   
-    <Card style={{ width: '15rem' , margin: "2rem" }}>
-        <img src={team} alt="" />
-      <Card.Body>
-        <Card.Title>Mohamed Ali</Card.Title>
-        <Card.Text className='text'>Head of UI/UX</Card.Text>
-        <img src={linkedin} alt="" />
-        <img src={Github} alt="" />
-        <img src={Behance} alt="" />
-      </Card.Body>
-    </Card>
-    <Card style={{ width: '15rem' , margin: "2rem 2rem" }}>
-        <img src={team} alt="" />
-      <Card.Body>
-        <Card.Title>Mohamed Ali</Card.Title>
-        <Card.Text className='text'>Web Development</Card.Text>
-        <img src={linkedin} alt="" />
-        <img src={Github} alt="" />
-        <img src={Behance} alt="" />
-      </Card.Body>
-    </Card>
-    </Row>
-    <Row  className='row row-team ms-4'>
-    <Card style={{ width: '15rem' , margin: "2rem 2rem" }}>
-        <img src={team} alt="" />
-      <Card.Body>
-        <Card.Title>Mohamed Ali</Card.Title>
-        <Card.Text className='text'>UI/UX Designer</Card.Text>
-        <img src={linkedin} alt="" />
-        <img src={Github} alt="" />
-        <img src={Behance} alt="" />
-      </Card.Body>
-    </Card>
-    <Card style={{ width: '15rem' , margin: "2rem 2rem" }}>
-        <img src={team} alt="" />
-      <Card.Body>
-        <Card.Title>Mohamed Ali</Card.Title>
-        <Card.Text className='text'>Graphic Design</Card.Text>
-        <img src={linkedin} alt="" />
-        <img src={Github} alt="" />
-        <img src={Behance} alt="" />
-      </Card.Body>
-    </Card>
-    <Card style={{ width: '15rem' , margin: "2rem 2rem" }}>
-        <img src={team} alt="" />
-      <Card.Body>
-        <Card.Title>Mohamed Ali</Card.Title>
-        <Card.Text className='text'>Frontend</Card.Text>
-        <img src={linkedin} alt="" />
-        <img src={Github} alt="" />
-        <img src={Behance} alt="" />
-      </Card.Body>
-    </Card>
-    <Card style={{ width: '15rem' , margin: "2rem 2rem" }}>
-        <img src={team} alt="" />
-      <Card.Body>
-        <Card.Title>Mohamed Ali</Card.Title>
-        <Card.Text className='text'>Backend</Card.Text>
-        <img src={linkedin} alt="" />
-        <img src={Github} alt="" />
-        <img src={Behance} alt="" />
-      </Card.Body>
-    </Card>
-    </Row>
+  return (
+    <div>
+      <h1 className='heading'>Our Team</h1>
+      <ButtonToolbar className='contant-center justify-content-center' aria-label="Toolbar with button groups">
+        <ButtonGroup className="department me-5" aria-label="First group">
+          <Button className='buttonGroup' variant='' active>All</Button>
+        </ButtonGroup>
+        <ButtonGroup className="department me-5" aria-label="Third group">
+          <Button className='buttonGroup' variant='outline-dark'>Owner</Button>
+        </ButtonGroup>
+        <ButtonGroup className="department me-5" aria-label="Third group">
+          <Button className='buttonGroup' variant='outline-dark'>UI/UX</Button>
+        </ButtonGroup>
+        <ButtonGroup className="department me-5" aria-label="Third group">
+          <Button className='buttonGroup' variant='outline-dark'>Front-End</Button>
+        </ButtonGroup>
+        <ButtonGroup className="department me-5" aria-label="Third group">
+          <Button className='buttonGroup' variant='outline-dark'>Back-End</Button>
+        </ButtonGroup>
+        <ButtonGroup className="department me-5" aria-label="Third group">
+          <Button className='buttonGroup' variant='outline-dark'>Flutter</Button>
+        </ButtonGroup>
+      </ButtonToolbar>
+      <Container className=' d-flex flex-column justify-content-center align-items-center'>
+        <Row className='row row-team ms-4'>
+          <Card style={{ width: '15rem', margin: "2rem" }}>
+            <img src={team} alt="" />
+            <Card.Body>
+              <Card.Title>Mohamed Ali</Card.Title>
+              <Card.Text className='text'>Graphic Design</Card.Text>
+              <img src={linkedin} alt="" />
+              <img src={Github} alt="" />
+              <img src={Behance} alt="" />
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '15rem', margin: "2rem" }}>
+            <img src={team} alt="" />
+            <Card.Body>
+              <Card.Title >Mohamed Ali</Card.Title>
+              <Card.Text className='text'>Head of Flutter</Card.Text>
+              <img src={linkedin} alt="" />
+              <img src={Github} alt="" />
+              <img src={Behance} alt="" />
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '15rem', margin: "2rem" }}>
+            <img src={team} alt="" />
+            <Card.Body>
+              <Card.Title>Mohamed Ali</Card.Title>
+              <Card.Text className='text'>Head of UI/UX</Card.Text>
+              <img src={linkedin} alt="" />
+              <img src={Github} alt="" />
+              <img src={Behance} alt="" />
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '15rem', margin: "2rem 2rem" }}>
+            <img src={team} alt="" />
+            <Card.Body>
+              <Card.Title>Mohamed Ali</Card.Title>
+              <Card.Text className='text'>Web Development</Card.Text>
+              <img src={linkedin} alt="" />
+              <img src={Github} alt="" />
+              <img src={Behance} alt="" />
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '15rem', margin: "2rem 2rem" }}>
+            <img src={team} alt="" />
+            <Card.Body>
+              <Card.Title>Mohamed Ali</Card.Title>
+              <Card.Text className='text'>Web Development</Card.Text>
+              <img src={linkedin} alt="" />
+              <img src={Github} alt="" />
+              <img src={Behance} alt="" />
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '15rem', margin: "2rem 2rem" }}>
+            <img src={team} alt="" />
+            <Card.Body>
+              <Card.Title>Mohamed Ali</Card.Title>
+              <Card.Text className='text'>Web Development</Card.Text>
+              <img src={linkedin} alt="" />
+              <img src={Github} alt="" />
+              <img src={Behance} alt="" />
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '15rem', margin: "2rem 2rem" }}>
+            <img src={team} alt="" />
+            <Card.Body>
+              <Card.Title>Mohamed Ali</Card.Title>
+              <Card.Text className='text'>Web Development</Card.Text>
+              <img src={linkedin} alt="" />
+              <img src={Github} alt="" />
+              <img src={Behance} alt="" />
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '15rem', margin: "2rem 2rem" }}>
+            <img src={team} alt="" />
+            <Card.Body>
+              <Card.Title>Mohamed Ali</Card.Title>
+              <Card.Text className='text'>Web Development</Card.Text>
+              <img src={linkedin} alt="" />
+              <img src={Github} alt="" />
+              <img src={Behance} alt="" />
+            </Card.Body>
+          </Card>
+        </Row>
+        
+        {/* <Row className='row row-team ms-4'>
+          <Card style={{ width: '15rem', margin: "2rem 2rem" }}>
+            <img src={team} alt="" />
+            <Card.Body>
+              <Card.Title>Mohamed Ali</Card.Title>
+              <Card.Text className='text'>UI/UX Designer</Card.Text>
+              <img src={linkedin} alt="" />
+              <img src={Github} alt="" />
+              <img src={Behance} alt="" />
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '15rem', margin: "2rem 2rem" }}>
+            <img src={team} alt="" />
+            <Card.Body>
+              <Card.Title>Mohamed Ali</Card.Title>
+              <Card.Text className='text'>Graphic Design</Card.Text>
+              <img src={linkedin} alt="" />
+              <img src={Github} alt="" />
+              <img src={Behance} alt="" />
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '15rem', margin: "2rem 2rem" }}>
+            <img src={team} alt="" />
+            <Card.Body>
+              <Card.Title>Mohamed Ali</Card.Title>
+              <Card.Text className='text'>Frontend</Card.Text>
+              <img src={linkedin} alt="" />
+              <img src={Github} alt="" />
+              <img src={Behance} alt="" />
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '15rem', margin: "2rem 2rem" }}>
+            <img src={team} alt="" />
+            <Card.Body>
+              <Card.Title>Mohamed Ali</Card.Title>
+              <Card.Text className='text'>Backend</Card.Text>
+              <img src={linkedin} alt="" />
+              <img src={Github} alt="" />
+              <img src={Behance} alt="" />
+            </Card.Body>
+          </Card>
+        </Row> */}
       </Container>
 
-        </div>
-      );
+    </div>
+  );
 }
 
 export default Team;
